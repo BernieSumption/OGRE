476a7d38aed4bbd62762e6eef1099302ffeacc32
Bernie Sumption <bernie@berniecode.com>
2010-12-03 11:08:27 +0100

    EDR TypeDomain mapping for all Java integer types is complete

---
+++ maven/ogre-engine/src/main/java/com/berniecode/ogre/server/pojods/NamedComparator.java
@@ -0,0 +1,24 @@
+
+package com.berniecode.ogre.server.pojods;
+
+import java.util.Comparator;
+
+import com.berniecode.ogre.enginelib.shared.Named;
+
+/**
+ * Compare two {@link Named} objects
+ *
+ * @author Bernie Sumption
+ */
+final class NamedComparator implements Comparator<Named> {
+	
+	public static final NamedComparator INSTANCE = new NamedComparator();
+	
+	// private constructor
+	private NamedComparator() {}
+
+	@Override
+	public int compare(Named o1, Named o2) {
+		return o1.getName().compareTo(o2.getName());
+	}
+}
---
+++ maven/ogre-xlanguage/src/main/java/com/berniecode/ogre/enginelib/shared/Named.java
@@ -0,0 +1,10 @@
+package com.berniecode.ogre.enginelib.shared;
+
+/**
+ * Something with a name
+ *
+ * @author Bernie Sumption
+ */
+public interface Named {
+	public String getName();
+}
