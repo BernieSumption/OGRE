0bfe2d6eaddf8a86c2c79d9fa86da0288e906c34
Bernie Sumption <bernie@berniecode.com>
2011-01-23 18:34:27 +0300

    Reorganised maven project structure and started friendgraph project

---
+++ maven/ogre-demo/demo-friendgraph/src/main/com/berniecode/ogre/demos/friendgraph/model/LikesRelationship.java
@@ -0,0 +1,20 @@
+package com.berniecode.ogre.demos.friendgraph.model;
+
+/**
+ * Expresses a one-way liking relationship
+ *
+ * @author Bernie Sumption
+ */
+public interface LikesRelationship {
+
+	/**
+	 * @return the person who does the liking
+	 */
+	public Person getSubject();
+	
+	/**
+	 * @return the person who is liked
+	 */
+	public Person getObject();
+
+}
---
+++ maven/ogre-demo/demo-friendgraph/src/main/com/berniecode/ogre/demos/friendgraph/model/Person.java
@@ -0,0 +1,11 @@
+package com.berniecode.ogre.demos.friendgraph.model;
+
+public interface Person {
+	
+	public String getName();
+	
+	public int getAge();
+	
+	public byte[] getPhotoJpeg();
+
+}
