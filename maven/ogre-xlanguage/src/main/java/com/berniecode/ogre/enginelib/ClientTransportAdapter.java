be9a664e91856778d470383b69850dca52d4f3a2
Bernie Sumption <bernie@berniecode.com>
2011-05-01 20:30:15 +1000

    Added copyright notice to all files

--- maven/ogre-xlanguage/src/main/java/com/berniecode/ogre/enginelib/ClientTransportAdapter.java
+++ maven/ogre-xlanguage/src/main/java/com/berniecode/ogre/enginelib/ClientTransportAdapter.java
@@ -1,13 +1,35 @@
+/*
+ * Copyright 2011 Bernie Sumption. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of conditions
+ * and the following disclaimer. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution. THIS SOFTWARE IS PROVIDED ``AS
+ * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * FREEBSD PROJECT OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ */
+
 package com.berniecode.ogre.enginelib;
 
 import com.berniecode.ogre.enginelib.platformhooks.NoSuchThingException;
 
 /**
- * A blocking request/response mechanism to fetch snapshots and type domains from an OGRE server.
+ * An adapter to connect an OGRE client with an OGRE server
+ * 
+ * <p>
+ * TODO this should be non-blocking, to take into account languages with no blocking IO
  * 
  * @author Bernie Sumption
  */
-//TODO this should be non-blocking, to take into account languages with no blocking IO
 public interface ClientTransportAdapter {
 
 	/**
@@ -16,10 +38,10 @@ public interface ClientTransportAdapter {
 	TypeDomain loadTypeDomain(String typeDomainId) throws NoSuchThingException;
 
 	/**
-	 * Load a snapshot of the state of an object graph 
+	 * Load a snapshot of the state of an object graph
 	 */
 	GraphUpdate loadObjectGraph(TypeDomain typeDomain, String objectGraphId) throws NoSuchThingException;
-	
+
 	/**
 	 * Subscribe to graph updates relating to a specific object graph
 	 */
